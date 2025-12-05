a586f40891ae231319ec74e55fc96e56f0ad639e
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2025-12-05 12:27:32 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from datetime import datetime
+import zoneinfo
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
+
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "US": "America/New_York",
+    "PE": "America/Lima",
+}
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
