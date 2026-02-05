b47b8290d370faebff350b880064cb1dfa94d9f3
Orlando Flores Teomitzi <orlando.floresti@gmail.com>
2026-02-04 18:10:20 -0600

    Add customer deletion endpoint with error handling

--- main.py
+++ main.py
@@ -38,6 +38,16 @@ async def read_customer(customer_id: int, session: SessionDep):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
     return customer_db
 
+@app.delete('/customers/{customer_id}')
+async def delete_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+    session.delete(customer_db)
+    session.commit()
+
+    return {"detail": "Customer deleted"}
+
 @app.post('/customers', response_model=Customer)
 async def create_customer(customer_data: CustomerCreate, session: SessionDep):
     customer = Customer.model_validate(customer_data.model_dump())
